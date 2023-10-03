347c21e32a0d4027cb0e3ca33f24b4cf6dcbf14d
Cash Myers <cmyers880@gmail.com>
2023-10-02 21:44:01 -0500

    Iniitial Commit

---
+++ client/build/precache-manifest.90e83a7aaec3c0c3d8231ebbb1cffbf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea4ff4fddc5e723c6892f7c1d6a43a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "000117fa31ae0aa66373",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ff2452c65fdd6eaad6fc",
+    "url": "/static/js/2.f1aa6e9a.chunk.js"
+  },
+  {
+    "revision": "be326db8c8d9b3aa36d942e163e338e7",
+    "url": "/static/js/2.f1aa6e9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "000117fa31ae0aa66373",
+    "url": "/static/js/main.1b00f9f6.chunk.js"
+  },
+  {
+    "revision": "4387b907a4d64303b5a2",
+    "url": "/static/js/runtime-main.fab42d54.js"
+  }
+]);
